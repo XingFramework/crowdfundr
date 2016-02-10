2699cf706da6a6068ab886a272d41cf181a2b80f
Paula Burke <paula.burke@gmail.com>
2016-02-09 17:48:33 -0800

    Creates project mapper.
    [#113318111]

---
+++ backend/app/mappers/project_mapper.rb
@@ -0,0 +1,17 @@
+class ProjectMapper < Xing::Mappers::Base
+  alias project record
+  alias project= record=
+
+  def record_class
+    Project
+  end
+
+  def assign_values(data_hash)
+    @project_data = data_hash
+    super
+  end
+
+  def update_record
+    project.assign_attributes(@project_data)
+  end
+end
