93e31a9fffa0f0ea5c1e866d44f6508b55e69574
Paula Burke <paula.burke@gmail.com>
2016-02-10 15:39:20 -0800

    Adds project edit state.
    Modifies controller name.
    
    [#113318111]

--- frontend/src/app/projects/projectsStates.js
+++ frontend/src/app/projects/projectsStates.js
@@ -6,7 +6,7 @@ export class ProjectsState {
     this.url = "/project/:id";
     this.templateUrl = "projects/project.tpl.html";
     this.controller = "ProjectsCtrl";
-    this.controllerAs = "project";
+    this.controllerAs = "projectCtrl";
   }
   @Resolve('resources', '$stateParams')
   project(resources, $stateParams) {
@@ -21,5 +21,19 @@ export class ProjectNewState {
     this.templateUrl = "projects/project-new.tpl.html";
     this.controller = "ProjectNewCtrl";
     this.controllerAs = "projectCtrl";
+  }
+}
+
+@State("root.inner.projectEdit")
+export class ProjectEditState {
+  constructor() {
+    this.url = "/edit/:id";
+    this.templateUrl = "projects/project-edit.tpl.html";
+    this.controller = "ProjectsEditCtrl";
+    this.controllerAs = "projectsCtrl";
+  }
+  @Resolve('resources', '$stateParams')
+  project(resources, $stateParams) {
+    return resources.project($stateParams).load();
   }
 }
