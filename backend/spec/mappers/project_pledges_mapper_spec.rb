d55d1f63ffe0cd94816a44e1cb1e6bee89e6de01
Peter Phan <peter.m.phan@gmail.com>
2016-02-11 14:26:00 -0800

    Start on project_pledges_mapper_spec.rb
    [#113318233]

--- backend/spec/mappers/project_pledges_mapper_spec.rb
+++ backend/spec/mappers/project_pledges_mapper_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe ProjectPledgesMapper, :type => :mappers do
+  let :project do
+    FactoryGirl.create(:project)
+  end
 
+  context 'valid data' do
+    it 'should create a new pledge in the list' do
+      expect do
+        mapper.save
+      end.to change { project.pledges.reload.count }.by(1)
+    end
+  end
 end
